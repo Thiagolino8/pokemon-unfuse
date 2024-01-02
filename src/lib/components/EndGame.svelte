3a29e6a391b22738952a1924ff29f9d48997febf
Thiago Lino Gomes <thiagolinog8@gmail.com>
2024-01-02 17:58:31 -0300

    Update EndGame.svelte

--- src/lib/components/EndGame.svelte
+++ src/lib/components/EndGame.svelte
@@ -15,6 +15,6 @@
 {#if game.state !== GameState.playing}
 	<dialog use:show transition:fade class="grid [&::backdrop]:bg-black/70 overflow-hidden bg-transparent gap-4 place-content-center">
 		<h2 class="text-4xl text-white font-bold">{endGameMessages[game.state]}</h2>
-		<button on:click={() => (game.state = GameState.playing)} class="btn btn-primary">Play again</button>
+		<button onclick={() => (game.state = GameState.playing)} class="btn btn-primary">Play again</button>
 	</dialog>
 {/if}
